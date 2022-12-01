bf2847b0398bbfa46195c01be17e3f4c2fbf5175
Khushnood <khushnoodhussain@fynd-external.com>
2022-12-01 14:49:43 +0530

    added cors fix

--- app/server.js
+++ app/server.js
@@ -21,7 +21,7 @@ const store = new mongoDBStrore({
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
-app.use(cors())
+app.use(cors({credentials: true, origin: true}))
 // app.use(cors({origin: "https://chat-app-ui-nu.vercel.app", credentials: true}))
 app.use(sessions({
   name : 'chat.sid',
